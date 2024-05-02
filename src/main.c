b80627a1914799a2f38f4056d9fef3b4273807f6
Azurioh <alan.cunin@epitech.eu>
2024-05-02 17:54:21 +0200

    (+) Create basic main function, currently doesn't take arguments File added: src/main.c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** Main function for the corewar project
+*/
+
+#include "../include/corewar.h"
+
+int main(void)
+{
+    return corewar();
+}
