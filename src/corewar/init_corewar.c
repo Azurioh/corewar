e454a9f8512b6110aa6d33e816aecf0771a780a7
Azurioh <alan.cunin@epitech.eu>
2024-05-02 17:54:00 +0200

    (+) Create function to init the corewar struct File added: src/corewar/init_corewar.c

---
+++ src/corewar/init_corewar.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2024
+** init_corewar.c
+** File description:
+** Function to init the corewar struct
+*/
+
+#include "../../include/corewar.h"
+
+static char *allocate_memory(void)
+{
+    char *memory_arena = malloc(sizeof(char) * MEM_SIZE);
+
+    if (!memory_arena) {
+        my_puterr("allocate_memory_arena: Memory allocation failed\n");
+        return NULL;
+    }
+    for (int i = 0; i < MEM_SIZE; i++) {
+        memory_arena[i] = 0;
+    }
+    return memory_arena;
+}
+
+corewar_t *init_corewar(void)
+{
+    corewar_t *corewar = malloc(sizeof(corewar_t));
+
+    if (!corewar) {
+        my_puterr("init_corewar: Memory allocation failed\n");
+        return NULL;
+    }
+    corewar->memory = allocate_memory();
+    return corewar;
+}
