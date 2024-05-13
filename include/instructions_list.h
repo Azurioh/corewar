0c3ae4b371904ee9ea5983660c9d41ede78769df
Azurioh <alan.cunin@epitech.eu>
2024-05-14 00:57:59 +0200

    (~) Update the parameters of the pointer function in the instructions_check struct and add the live and add instruction File modified: include/instructions_list.h

--- include/instructions_list.h
+++ include/instructions_list.h
@@ -7,18 +7,18 @@
 
 #ifndef INSTRUCTIONS_LIST_H
     #define INSTRUCTIONS_LIST_H
-    #include "robot.h"
+    #include "corewar.h"
 
 typedef struct instructions_check_s {
     int id;
-    void (*function)(robot_t *, char *);
+    void (*function)(corewar_t *, robot_t *);
 } instructions_check_t;
 
 const instructions_check_t INSTRUCTIONS_LIST[] = {
-    { 0x01, NULL },
+    { 0x01, &live },
     { 0x02, NULL },
     { 0x03, NULL },
-    { 0x04, NULL },
+    { 0x04, &add },
     { 0x05, NULL },
     { 0x06, NULL },
     { 0x07, NULL },
