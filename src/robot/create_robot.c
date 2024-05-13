7275d47754fae1dba0a091ed3e0350c1c6647faa
Azurioh <alan.cunin@epitech.eu>
2024-05-13 21:21:58 +0200

    (~) Update the function to init a robot struct File modified: src/robot/create_robot.c

--- src/robot/create_robot.c
+++ src/robot/create_robot.c
@@ -6,21 +6,58 @@
 */
 #include "../../include/corewar.h"
 
-robot_t *create_robot(char *filepath, int start, int number)
+static bool init_registers(robot_t *robot)
 {
-    robot_t *robot = init_robot();
-    char *buffer = NULL;
+    char *registers = malloc(sizeof(char) * REG_NUMBER);
+
+    if (!registers) {
+        my_puterr("init_registers: Memory allocation failed\n");
+        robot->registers = NULL;
+        return false;
+    }
+    for (int i = 0; i < REG_NUMBER; i++) {
+        registers[i] = 0;
+    }
+    robot->registers = registers;
+    return true;
+}
+
+static bool load_robot_informations(robot_t *robot, char *buffer, int start,
+    int number) {
     static unsigned int nb_player = 0;
 
-    if (robot == NULL || read_file(filepath, &buffer) == -1)
-        return robot;
     robot->name = my_strndup(&(buffer[4]), PROG_NAME_LENGTH);
+    if (robot->name == false)
+        return false;
+    if (init_registers(robot) == false)
+        return false;
     robot->start_index_in_memory = start;
-    if (number != -1)
+    robot->is_alive = false;
+    robot->is_dead = false;
+    if (number != -1) {
         robot->nb_player = number;
-    else {
+    } else {
         robot->nb_player = nb_player;
-        nb_player += 1;
+        nb_player++;
+    }
+    return true;
+}
+
+robot_t *create_robot(char *filepath, int start, int number)
+{
+    robot_t *robot = malloc(sizeof(robot_t));
+    char *buffer = NULL;
+
+    if (!robot) {
+        return NULL;
+    }
+    if (read_file(filepath, &buffer) == -1) {
+        free_robot(robot);
+        return NULL;
+    }
+    if (load_robot_informations(robot, buffer, start, number) == false) {
+        free_robot(robot);
+        return NULL;
     }
     return robot;
 }
