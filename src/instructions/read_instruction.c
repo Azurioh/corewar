be5b38efedc9aecf89ad2bd03c473ca66b3345ab
Azurioh <alan.cunin@epitech.eu>
2024-05-14 00:56:32 +0200

    (~) Update the read instruction function with new parameters and update the condition if the robot must wait File modified: src/instructions/read_instruction.c

--- src/instructions/read_instruction.c
+++ src/instructions/read_instruction.c
@@ -8,21 +8,21 @@
 #include "../../include/corewar.h"
 #include "../../include/instructions_list.h"
 
-void read_instruction(robot_t *robot, char *memory)
+void read_instruction(corewar_t *corewar, robot_t *robot)
 {
-    if (!robot || !memory) {
+    if (!corewar || !robot) {
         return;
     }
     if (robot->is_dead == true) {
         return;
     }
-    if (robot->nb_cycles_to_wait != 0) {
+    if (robot->nb_cycles_to_wait > 0) {
         robot->nb_cycles_to_wait--;
         return;
     }
     for (int i = 0; INSTRUCTIONS_LIST[i].id != 0; i++) {
-        if (memory[robot->reading_index] == INSTRUCTIONS_LIST[i].id) {
-            INSTRUCTIONS_LIST[i].function(robot, memory);
+        if (corewar->memory[robot->reading_index] == INSTRUCTIONS_LIST[i].id) {
+            INSTRUCTIONS_LIST[i].function(corewar, robot);
             return;
         }
     }
