316f739d94b1358186e63253e23ef2011a94c909
Azurioh <alan.cunin@epitech.eu>
2024-05-14 16:44:18 +0200

    (~) Update the st function with a new way to manage errors File modified: src/instructions/st.C

--- src/instructions/st.c
+++ src/instructions/st.c
@@ -7,10 +7,10 @@
 
 #include "../../include/corewar.h"
 
-static char *parse_st_args(unsigned char *memory, char *coding_byte,
+static int *parse_st_args(unsigned char *memory, char *coding_byte,
     int read_index)
 {
-    char *arguments = malloc(sizeof(char) * 4);
+    int *arguments = malloc(sizeof(int) * 4);
 
     if (!arguments) {
         my_puterr("parse_st_args: Memory allocation failed\n");
@@ -19,43 +19,55 @@ static char *parse_st_args(unsigned char *memory, char *coding_byte,
     arguments[0] = get_address_value(memory, read_index + 2);
     arguments[1] = get_address_value(memory, read_index + 3);
     if (coding_byte[1] == T_IND) {
-        arguments[1] = (arguments[1] << 8);
+        arguments[1] = arguments[1] << 8;
         arguments[1] += get_address_value(memory, read_index + 4);
     }
     arguments[2] = 0;
-    free(coding_byte);
     return arguments;
 }
 
-static void store_st_result(unsigned char *memory, char *arguments,
-    char *coding_byte,
-    robot_t *robot)
+static void move_robot_index_st_command(char *coding_byte, robot_t *robot)
 {
     if (coding_byte[1] == T_IND) {
-        memory[robot->read_index + arguments[1] % IDX_MOD] = arguments[0];
         robot->read_index = get_address(robot->read_index + 5);
     } else {
-        robot->registers[(int)arguments[1]] = arguments[0];
         robot->read_index = get_address(robot->read_index + 4);
+    }
+}
+
+static void store_st_result(unsigned char **memory, int *arguments,
+    char *coding_byte, robot_t *robot)
+{
+    int value;
+
+    if (register_is_valid((int)arguments[0]) == false) {
+        return;
+    }
+    value = robot->registers[(int)arguments[0] - 1];
+    if (coding_byte[1] == T_IND) {
+        (*memory)[get_address(robot->read_index + (int)arguments[1] % IDX_MOD)]
+            = value;
+    } else {
+        if (register_is_valid((int)arguments[1]) == false) {
+            return;
+        }
+        robot->registers[(int)arguments[1] - 1] = value;
     }
 }
 
 void st_instruction(corewar_t *corewar, robot_t *robot)
 {
     char *c_byte = read_coding_byte(corewar->memory[robot->read_index + 1]);
-    char *arguments = parse_st_args(corewar->memory, c_byte,
+    int *arguments = parse_st_args(corewar->memory, c_byte,
         robot->read_index);
 
     if (!arguments) {
-        if (c_byte[1] == T_IND) {
-            robot->read_index = get_address(robot->read_index + 5);
-        } else {
-            robot->read_index += get_address(robot->read_index + 4);
-        }
+        move_robot_index_st_command(c_byte, robot);
         free(c_byte);
         return;
     }
-    store_st_result(corewar->memory, arguments, c_byte, robot);
+    store_st_result(&corewar->memory, arguments, c_byte, robot);
+    move_robot_index_st_command(c_byte, robot);
     free(arguments);
     free(c_byte);
 }
