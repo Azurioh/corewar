d07eaef8b3c8bcd6a461af1644b16294222b9421
Azurioh <alan.cunin@epitech.eu>
2024-05-14 09:55:55 +0200

    (~) Update the live instruction with the new function to read the memory File modified: src/instructions/live.c

--- src/instructions/live.c
+++ src/instructions/live.c
@@ -10,12 +10,13 @@ void live(corewar_t *corewar, robot_t *robot)
 {
     char *number_string = NULL;
     int nb_player;
+    char value = get_address_value(corewar->memory, robot->read_index + 2);
 
-    number_string = my_strndup(&corewar->memory[robot->reading_index + 2], 4);
+    number_string = my_strndup(&value, 4);
     nb_player = my_getnbr(number_string);
     free(number_string);
     robot->nb_cycles_to_wait = get_operation_info("live").nbr_cycles;
-    robot->reading_index += 6;
+    robot->read_index += 6;
     robot->is_alive = true;
     my_printf("The player %d(%s)is alive.", nb_player, robot->name);
     corewar->last_robot_alive = robot;
