e790717e4e93fa9d9e56a8e149124d7e660a752a
Azurioh <alan.cunin@epitech.eu>
2024-05-15 19:38:47 +0200

    (~) Fix indentation of the robot index in the ld instruction File modified: src/instructions/ld.c

--- src/instructions/ld.c
+++ src/instructions/ld.c
@@ -11,27 +11,27 @@ int get_value(unsigned char *memory, robot_t *robot, int *index_reg)
     char *type_tab = read_coding_byte(memory[robot->read_index + 1]);
     int index;
 
-    if (type_tab[1] == T_DIR) {
+    if (type_tab[0] == T_DIR) {
         free(type_tab);
-        *index_reg = 6;
+        *index_reg += 4;
         return convert_4bytes(memory, robot->read_index + 1) % IDX_MOD;
     }
     index = convert_2bytes(memory, robot->read_index + 1);
-    *index_reg = 4;
+    *index_reg += 2;
     return get_address_value(memory, index) % IDX_MOD;
 }
 
 void ld_instruction(corewar_t *corewar, robot_t *robot)
 {
     int value;
-    int index_of_reg_nb;
+    int index_of_reg_nb = 2;
     int register_index;
 
     value = get_value(corewar->memory, robot, &index_of_reg_nb);
     register_index = get_address_value(corewar->memory,
         robot->read_index + index_of_reg_nb);
     robot->registers[register_index] = value - 1;
-    robot->read_index = get_address(robot->read_index + index_of_reg_nb);
+    robot->read_index = get_address(robot->read_index + index_of_reg_nb + 1);
     robot->carry = 0;
     if (value == 0)
         robot->carry = 1;
