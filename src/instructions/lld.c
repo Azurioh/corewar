23b9e70d6d70f0b5634e2c8145d0e0774c2d014f
milinmatteo <matteo.milin@epitech.eu>
2024-05-15 21:36:19 +0200

    [0] Fix ld and lld instructions

--- src/instructions/lld.c
+++ src/instructions/lld.c
@@ -8,30 +8,31 @@
 
 int get_long_value(unsigned char *memory, robot_t *robot, int *index_reg)
 {
-    char *type_tab = read_coding_byte(robot->read_index + 1);
+    char *type_tab = read_coding_byte(memory[robot->read_index + 1]);
     int index;
 
-    if (type_tab[1] == T_DIR) {
+    if (type_tab[0] == T_DIR) {
         free(type_tab);
-        *index_reg = 6;
+        *index_reg += 4;
         return convert_4bytes(memory, robot->read_index + 1);
     }
     index = convert_2bytes(memory, robot->read_index + 1);
-    *index_reg = 4;
+    index += robot->read_index;
+    *index_reg += 2;
     return get_address_value(memory, index);
 }
 
 void lld_instruction(corewar_t *corewar, robot_t *robot)
 {
     int value;
-    int index_of_reg_nb;
+    int index_of_reg_nb = 2;
     int register_index;
 
     value = get_long_value(corewar->memory, robot, &index_of_reg_nb);
     register_index = get_address_value(corewar->memory,
         robot->read_index + index_of_reg_nb);
-    robot->registers[register_index] = value - 1;
-    robot->read_index = get_address(robot->read_index + index_of_reg_nb);
+    robot->registers[register_index - 1] = value;
+    robot->read_index = get_address(robot->read_index + index_of_reg_nb + 1);
     robot->carry = 0;
     if (value == 0)
         robot->carry = 1;
