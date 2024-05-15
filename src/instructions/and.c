aac523841cdf8d219a8dca279ebecb4bc45c67a4
Azurioh <alan.cunin@epitech.eu>
2024-05-15 19:39:03 +0200

    (~) Fix the and instruction File modified: src/instructions/and.c

--- src/instructions/and.c
+++ src/instructions/and.c
@@ -11,23 +11,24 @@ static int *read_and_args(unsigned char *memory, robot_t *robot,
     char *coding_byte)
 {
     int *arguments = malloc(sizeof(int) * 4);
-    int move = 0;
+    int move = 1;
 
     if (!arguments) {
         my_puterr("read_and_args: Memory allocation failed\n");
         free(coding_byte);
         return NULL;
     }
-    move++;
     for (int i = 0; i < 2; i++) {
         arguments[i] = read_parameters_with_cbyte(memory, coding_byte[i],
             robot->read_index + move);
-        if (coding_byte[i] == T_IND)
+        if (coding_byte[i] == T_IND) {
             arguments[i] = robot->read_index + arguments[i] % IDX_MOD;
+        }
         move += get_movement_size_with_cbyte(coding_byte[i]);
     }
+    move++;
     arguments[2] = get_address_value(memory, robot->read_index + move);
-    robot->read_index += move + 2;
+    arguments[3] = move + 1;
     return arguments;
 }
 
@@ -50,20 +51,20 @@ void and_instruction(corewar_t *corewar, robot_t *robot)
     int *args = read_and_args(corewar->memory, robot, c_byte);
     int result;
 
-    if (!args) {
+    if (!args)
         return;
-    }
     robot->carry = 0;
     if (check_and_args_are_valid(args, c_byte) == false) {
+        robot->read_index = get_address(robot->read_index + args[3]);
         free(c_byte);
         free(args);
         return;
     }
     result = args[0] & args[1];
-    robot->registers[args[2]] = result;
-    if (result == 0) {
+    robot->registers[args[2] - 1] = result;
+    robot->read_index = get_address(robot->read_index + args[3]);
+    if (result == 0)
         robot->carry = 1;
-    }
     free(c_byte);
     free(args);
 }
