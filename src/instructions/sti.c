099a178dc7fc6c728fa3b73b0b71cba861d16d4f
milinmatteo <matteo.milin@epitech.eu>
2024-05-15 23:15:41 +0200

    [0] Fix sti instruction

--- src/instructions/sti.c
+++ src/instructions/sti.c
@@ -6,18 +6,14 @@
 */
 #include "../../include/corewar.h"
 
-int get_register_value(robot_t *robot, unsigned char *memory, int index)
+int get_register_value(robot_t *robot, int index)
 {
-    int register_index;
     int register_value;
 
-    if (register_is_valid(memory[get_address(robot->read_index + index)])
-        == false) {
+    if (register_is_valid(index) == false) {
         return -1;
     }
-    register_index = get_address_value(memory,
-        get_address(robot->read_index + index));
-    register_value = robot->registers[register_index];
+    register_value = robot->registers[index - 1];
     return register_value;
 }
 
@@ -27,15 +23,14 @@ int get_nparameter_value(robot_t *robot, unsigned char *memory, char c_byte,
     int value;
 
     if (c_byte == T_REG) {
-        value = get_register_value(robot, memory,
-            get_address(robot->read_index + *index));
+        value = get_register_value(robot,
+            get_address_value(memory, robot->read_index + *index));
+        *index += 1;
         if (value == -1)
             return -1;
-        *index += 4;
     } else {
-        value = get_address_value(memory,
-            get_address(robot->read_index + *index));
-        *index += 7;
+        value = convert_4bytes(memory, *index - 1);
+        *index += 4;
     }
     return value;
 }
@@ -45,31 +40,24 @@ int get_sti_address_to_stock(robot_t *robot, unsigned char *memory,
 {
     int first_value;
     int second_value;
-    int index = 3;
 
-    if (c_byte[1] == T_REG)
-        *param_size += 1;
-    else
-        *param_size += 2;
-    if (c_byte[2] == T_REG)
-        *param_size += 1;
-    else
-        *param_size += 2;
-    first_value = get_nparameter_value(robot, memory, c_byte[1], &index);
-    second_value = get_nparameter_value(robot, memory, c_byte[2], &index);
-    return first_value + second_value;
+    first_value = get_nparameter_value(robot, memory, c_byte[1], param_size);
+    second_value = get_nparameter_value(robot, memory, c_byte[2], param_size);
+    return (first_value + second_value) % IDX_MOD;
 }
 
 void sti_instruction(corewar_t *corewar, robot_t *robot)
 {
     char *c_byte = read_coding_byte(corewar->memory[robot->read_index + 1]);
     int param_size = 3;
-    int value_to_store = get_register_value(robot, corewar->memory,
+    int value_to_store = get_register_value(robot,
         get_address(robot->read_index + 2));
     int address = get_sti_address_to_stock(robot, corewar->memory, c_byte,
         &param_size);
 
     robot->read_index = get_address(robot->read_index + param_size);
+    if (value_to_store == -1)
+        return;
     corewar->memory[get_address(address)] = value_to_store;
     return;
 }
