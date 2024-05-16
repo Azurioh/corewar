c5e9ea5c1846fe9ed927f7b1959010269fbface6
milinmatteo <matteo.milin@epitech.eu>
2024-05-16 23:18:47 +0200

    [+] Add the name of the robot who writed in the memory

--- bonus/src/utils/register_to_memory.c
+++ bonus/src/utils/register_to_memory.c
@@ -7,12 +7,13 @@
 
 #include "../../include/corewar.h"
 
-void register_to_memory(corewar_t *corewar, int index, int value)
+void register_to_memory(corewar_t *corewar, int index, int value, robot_t *robot)
 {
     int shift = 24;
 
     for (int i = 0; i < 4; i++) {
         corewar->memory[get_address(index + i)].memory = value >> shift;
+        corewar->memory[get_address(index + i)].author = robot->name;
         shift -= 8;
     }
     return;
