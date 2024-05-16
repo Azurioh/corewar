f0ffe4cd104389e3701ebfbbfd4cfa6447577582
milinmatteo <matteo.milin@epitech.eu>
2024-05-16 21:08:50 +0200

    [0] Fix register_to_memory

--- src/utils/register_to_memory.c
+++ src/utils/register_to_memory.c
@@ -9,9 +9,11 @@
 
 void register_to_memory(corewar_t *corewar, int index, int value)
 {
-    int *value_shifted = convert_to_4bytes(value);
+    int shift = 24;
 
     for (int i = 0; i < 4; i++) {
-        corewar->memory[get_address(index + i)] = value_shifted[i];
+        corewar->memory[index + i] = value >> shift;
+        shift -= 8;
     }
+    return;
 }
