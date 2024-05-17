7c97b5479a9848288a1923b0b6abce520b3e2e04
alan.cunin@epitech.eu <alan.cunin@epitech.eu>
2024-05-17 00:11:17 +0000

    (~) Fix build error

--- bonus/src/instructions/aff.c
+++ bonus/src/instructions/aff.c
@@ -8,9 +8,6 @@
 
 void aff(corewar_t *corewar, robot_t *robot)
 {
-    int reg_index;
-
-    reg_index = get_address_value(corewar->memory, robot->read_index + 2);
     robot->read_index = get_address(robot->read_index + 3);
     return;
 }
